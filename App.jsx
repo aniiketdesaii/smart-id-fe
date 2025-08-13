f83e72cf2e5b3a20f0d08f2c15c6e8942c15f045
Aniket Desai <aniketgdesai@gmail.com>
2025-08-13 21:15:11 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import UploadPage from './pages/UploadPage';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/upload" element={<UploadPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
